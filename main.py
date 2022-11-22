84a76669b4df5e1b4defc08fd71a3625d96a165b
jadirrlima@gmail.com <@Wilson20@>
2022-11-22 16:46:37 -0400

    Implementação do Gráfico na planilha

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from datetime import date
 
+
+from openpyxl.chart import LineChart, Reference
+from openpyxl.drawing.image import Image
+from openpyxl.styles import Font, PatternFill, Alignment
 from openpyxl.workbook import Workbook
 
 # acao = input("Qual a código da Ação que vc quer processar? ")
@@ -36,5 +40,48 @@ for linha in linhas:
 
     indice += 1
 
+planilha_grafico = workbook.create_sheet("Gráfico")
+workbook.active = planilha_grafico
+
+
+# Mesclagem de células
+planilha_grafico.merge_cells("A1:T2")
+cabecalho = planilha_grafico['A1']
+cabecalho.font = Font(b=True, sz=18, color="FFFFFF")
+cabecalho.fill = PatternFill("solid", fgColor="07838f")
+cabecalho.alignment = Alignment(vertical="center", horizontal="center")
+cabecalho.value = "Histórico de Cotações"
+
+grafico = LineChart()
+grafico.width = 33.87
+grafico.height = 14.82
+grafico.title = f"Cotações - {acao}"
+grafico.x_axis.title = "Data da Cotação"
+grafico.y_axis.title = "Valor da Cotação"
+
+referencia_cotacoes = Reference(planilha_ativa, min_col=2, min_row=2, max_col=4, max_row=indice)
+referencia_datas = Reference(planilha_ativa, min_col=1, min_row=2, max_col=1, max_row=indice)
+grafico.add_data(referencia_cotacoes)
+grafico.set_categories(referencia_datas)
+
+linha_cotacoes = grafico.series[0]
+linha_bb_inferior = grafico.series[1]
+linha_bb_superior = grafico.series[2]
+
+linha_cotacoes.graphicalProperties.line.width = 0
+linha_cotacoes.graphicalProperties.line.solidFill = '0a55ab'
+
+linha_bb_inferior.graphicalProperties.line.width = 0
+linha_bb_inferior.graphicalProperties.line.solidFill = 'a61508'
+
+linha_bb_superior.graphicalProperties.line.width = 0
+linha_bb_superior.graphicalProperties.line.solidFill = '12a154'
+
+planilha_grafico.add_chart(grafico, "A3")
+
+
+imagem = Image('./recursos/logo.png')
+planilha_grafico.merge_cells("I32:L35")
+planilha_grafico.add_image(imagem, 'I32')
 
 workbook.save("./saida/Planilha.xlsx")
